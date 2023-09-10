18492b19c8695353e2f597481a8269ebde15c4e2
John Connolly (Credera) <john.connolly@sita.aero>
2023-09-10 14:29:52 +0100

    added test

--- client/src/pages/openRoles/OpenRoles.test.js
+++ client/src/pages/openRoles/OpenRoles.test.js
@@ -3,8 +3,17 @@ import { render, fireEvent } from '@testing-library/react';
 import OpenRoles from './OpenRoles';
 import { BrowserRouter } from 'react-router-dom';
 
-test('Button should render with initial text and show RoleFilter when clicked', () => {
-  const { getByText, queryByText } = render( <BrowserRouter><OpenRoles /></BrowserRouter> );
+test('Button should render with Show filter text', () => {
+  const { getByText } = render( <OpenRoles />);
+  
+  const showFilterButton = getByText('Show Filter');
+  expect(showFilterButton).toBeInTheDocument();
+ 
+  
+  })
+
+test('Button should render and filter should not', () => {
+  const { getByText, queryByText } = render( <OpenRoles /> );
   
   const showFilterButton = getByText('Show Filter');
   expect(showFilterButton).toBeInTheDocument();
@@ -12,11 +21,21 @@ test('Button should render with initial text and show RoleFilter when clicked', 
   const filterByRoleHeading = queryByText('Filter by Role');
   expect(filterByRoleHeading).toBeNull();
   
-  fireEvent.click(showFilterButton);
+  })
+
   
-  const hideFilterButton = getByText('Hide Filter');
-  expect(hideFilterButton).toBeInTheDocument();
+  test('when show filter button is clicked the filter component should render on screen', () => {
+    const { getByText } = render( <BrowserRouter><OpenRoles /></BrowserRouter> );
+
+    const showFilterButton = getByText('Show Filter');
+
+    fireEvent.click(showFilterButton);
   
-  const roleFilterHeading = getByText('Filter by Role');
-  expect(roleFilterHeading).toBeInTheDocument();
-  });
+    const hideFilterButton = getByText('Hide Filter');
+    expect(hideFilterButton).toBeInTheDocument();
+    
+    const roleFilterHeading = getByText('Filter by Role');
+    expect(roleFilterHeading).toBeInTheDocument();
+    });
+
+ 
